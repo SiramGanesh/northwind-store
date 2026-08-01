5d5412817672b27096770d5d1f07efdfc9bf3efb
Siram Ganesh <navadeepsaiganeshsiram@gmail.com>
2026-08-01 14:36:20 +0530

    feat: add OrdersPage component with order management functionality and loading/error handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { Show, SignInButton, SignUpButton, UserButton, useAuth } from '@clerk/react'
 import PageLoader from './components/PageLoader';
 import Layout from './components/Layout';
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router';
 import HomePage from './pages/HomePage';
 import CartPage from './pages/CartPage';
+import OrdersPage from './pages/OrdersPage';
 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/cart' element={<CartPage />} />
-
+          <Route path='/orders' element={isSignedIn ? <OrdersPage /> : <Navigate to={'/'} replace />} />
         </Routes>
       </Layout>
     </>
