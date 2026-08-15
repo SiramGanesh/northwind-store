1dc9d809957af33756efd1b66e53f79b043441f6
Siram Ganesh <navadeepsaiganeshsiram@gmail.com>
2026-08-15 10:54:10 +0530

    feat: add video support for order calls with OrderVideoPage and related hooks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import { SentryDemoPage } from './pages/SentryDemoPage';
 import OrderDetailPage from './pages/OrderDetailPage';
 import OrderSummaryPage from './pages/OrderSummaryPage';
 import OrderChatPage from './pages/OrderChatPage';
+import OrderVideoPage from './pages/OrderVideoPage';
 
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
           <Route path='/orders' element={isSignedIn ? <OrdersPage /> : <Navigate to={'/'} replace />} />
           <Route path='/checkout/return' element={<CheckoutReturnPage />} />
           <Route path='/demo-sentry' element={<SentryDemoPage />} />
+          <Route path='/orders/:id/call' element={isSignedIn ? <OrderVideoPage/> : <Navigate to={'/'} replace />} />
           <Route path='/orders/:id' element={<OrderDetailPage />} >
             <Route index element={<OrderSummaryPage />} />
             <Route path='chat' element={<OrderChatPage />} />
